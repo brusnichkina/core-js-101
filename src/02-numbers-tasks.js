3d700547a3a749d28306ea65037bff942f1a464e
brusnichkina <bru_snichkina@icloud.com>
2023-10-06 20:37:15 +0300

    feat: complete number task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -133,8 +133,10 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  /* throw new Error('Not implemented'); */
+  const valueString = value.toString();
+  return valueString.slice(-1);
 }
 
 
@@ -149,8 +151,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+/*  throw new Error('Not implemented'); */
+  return +value;
 }
 
 /**
@@ -166,8 +169,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  /* throw new Error('Not implemented'); */
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -188,8 +192,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  /* throw new Error('Not implemented'); */
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
@@ -209,8 +214,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+/* throw new Error('Not implemented'); */
+  for (let i = 2; i <= n / 2; i += 1) {
+    if (!(n % i)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -228,8 +239,13 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  /* throw new Error('Not implemented'); */
+  const res = Number(value);
+  if (typeof res === 'number' && !Number.isNaN(res)) {
+    return res;
+  }
+  return def;
 }
 
 module.exports = {
